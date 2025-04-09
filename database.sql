fe3a10edfb8943430aa7aca734ac818587dc2331
zihadurrahmandev <zihadurrahman.dev@gmail.com>
2025-04-09 21:21:25 +0600

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,4 @@
+INSERT INTO `settings` (`id`, `key`, `value`, `created_at`, `updated_at`) VALUES
+(12, 'tzsmoney_withdraw_status', '0', '2023-12-11 09:01:42', '2025-04-09 09:08:28'),
+(13, 'tzsmoney_api_key', 'ADFGASF', '2023-12-11 09:01:42', '2025-04-08 02:17:43'),
+(14, 'tzsmoney_minimum_withdraw', '250', '2023-12-11 09:01:42', '2025-04-09 09:08:28');
